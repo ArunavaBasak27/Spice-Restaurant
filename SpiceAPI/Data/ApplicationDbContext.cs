3be3bd178a080ecd8816dece9756a9244c77926d
ArunavaBasak27 <arunavadev99@gmail.com>
2023-04-11 12:45:59 +0530

    Adding Category Model

---
+++ SpiceAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpiceAPI.Models;
+#nullable disable
+
+namespace SpiceAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ SpiceAPI/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+#nullable disable
+namespace SpiceAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
